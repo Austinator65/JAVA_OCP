b688a526d8c11e88547e846357de0a8e02d5bc47
Austinator65 <atisomofokeng167@gmail.com>
2023-10-27 15:48:15 +0200

    first commit

---
+++ src/MainSceneController.java
@@ -0,0 +1,23 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+
+public class MainSceneController {
+
+    @FXML
+    private PasswordField PasswordField;
+
+    @FXML
+    private PasswordField UserNameField;
+
+    @FXML
+    void LoginBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SignUpBtn(ActionEvent event) {
+
+    }
+
+}
